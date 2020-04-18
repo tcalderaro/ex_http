553b70da363c52d695a59251abd471851d3f3532
Tiago Calderaro <tiagoocalderaro@hotmail.com>
2020-04-18 15:33:59 -0300

    first commit

---
+++ calculo.js
@@ -0,0 +1,7 @@
+
+var http = require("http");
+var cilindro = require('./cilindro.js');
+
+http.createServer(function(req,res){
+    res.end("Um cilíndro com raio 2 e altura 4 possui volume igual a "+cilindro.volume(2,4)+" e área externa igual a "+cilindro.area(2,4)+".");
+}).listen(8182);
